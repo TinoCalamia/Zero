eaaaafa6e12484544fc6dfc373ec6e482a4d1e54
Agostino Calamia <agostino.calamia@n26.com>
2020-12-26 22:37:35 +0100

    add worker

--- worker.py
+++ worker.py
@@ -3,15 +3,10 @@ import os
 
 import redis
 from rq import Worker, Queue, Connection
-import urllib.parse as up
 
 listen = ["high", "default", "low"]
 
-redis_url = os.getenv("REDISTOGO_URL")
-
-up.uses_netloc.append("redis")
-url = up.urlparse(redis_url)
-# conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 
 conn = redis.from_url(redis_url)
 
